db0bdf46c0fd103b74a240f581726a5f7b9e791c
Your Name <davidygp@outlook.com>
2019-05-12 18:30:06 +0800

    Added GetContactIntent

--- app.py
+++ app.py
@@ -11,29 +11,35 @@ app = Flask(__name__)
 # Intent Handlers funcs : END
 # ***************************
 def getLocationIntentHandler():
-    return "It can be found at:\n Please visit https://www.iss.nus.edu.sg/about-us/getting-to-nus-iss for more info"
+    return "The Heng Mui Keng Campus can be found at: 29 Heng Mui Keng Terrace, Singapore 119620. Please visit https://www.iss.nus.edu.sg/about-us/getting-to-nus-iss for more info"
+
+def getContactIntentHandler():
+    return "For General Enquiries - isstraining@nus.edu.sg, For Graduate Programme Enquiries
+Application & Admission - 6601 3161 / 6516 2516 / 6516 6153 / 6516 6769 / 6516 1047 isspostgrad@nus.edu.sg"
 
 # *****************************
 # WEBHOOK MAIN ENDPOINT : START
 # *****************************
 @app.route('/', methods=['POST'])
 def webhook():
-   req = request.get_json(silent=True, force=True)
-   intent_name = req["queryResult"]["intent"]["displayName"]
-   print(req)
+    req = request.get_json(silent=True, force=True)
+    intent_name = req["queryResult"]["intent"]["displayName"]
+    print(req)
  
-   if intent_name == "GetLocationIntent" :
-       respose_text = getLocationIntentHandler()
-   else:
-       respose_text = "No intent matched"
-   # Branching ends here
+    if intent_name == "GetLocationIntent" :
+        respose_text = getLocationIntentHandler()
+    elif intent_Name == "GetContactIntent" :
+        reponse_text = getContactIntentHandler()   
+    else:
+        respose_text = "No intent matched"
+    # Branching ends here
 
-   # Finally sending this response to Dialogflow.
-   return make_response(jsonify({'fulfillmentText': respose_text}))
+    # Finally sending this response to Dialogflow.
+    return make_response(jsonify({'fulfillmentText': respose_text}))
 
 # ***************************
 # WEBHOOK MAIN ENDPOINT : END
 # ***************************
 
 if __name__ == '__main__':
-   app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
